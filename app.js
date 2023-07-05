649e8f2172fafbd2b79c8f8cb50fc4cc620cf745
sumit <mazumdarsumit3@gmail.com>
2023-07-05 21:45:48 +0530

    created config/db.js and added MONGODB_URI and established connection with DB in atlas

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ require("dotenv").config(); //enabled dotenv
 const express = require("express");
 // import express from 'express';
 
+const connectToDb = require("./config/db.js");
+
 const app = express();
+
+// init connection to db
+
+connectToDb();
 
 app.get("/", (req, res) => {
   res.send("Hello world !");
