4f1180484bceea9bfe9a4cd2902b5d7307748eb1
sumit <mazumdarsumit3@gmail.com>
2023-07-05 22:54:39 +0530

    added controller/userController.js and routes/userRoutes.js

--- app.js
+++ app.js
@@ -7,20 +7,23 @@ const cors = require("cors"); // allow cross origin request
 
 const connectToDb = require("./config/db.js");
 
+const app = express();
 // Express middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
-const app = express();
-
 // init connection to db
 
 connectToDb();
 
-app.get("/", (req, res) => {
-  res.send("Hello world !");
-});
+const userRoutes = require("./routes/userRoutes.js");
+
+app.get("/", userRoutes);
+
+// app.get("/", (req, res) => {
+//   res.send("Hello world !");
+// });
 
 module.exports = app; // no need to enable module in package in json
---
+++ routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const { home } = require("../controllers/userController.js");
+
+const router = express.Router();
+
+router.get("/", home);
+
+module.exports = router;
