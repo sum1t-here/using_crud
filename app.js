a38555cbf7360600d61ff0fbd9b006541d51dcef
sumit <mazumdarsumit3@gmail.com>
2023-07-05 20:56:55 +0530

    created server.js , initialised .env in app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+require("dotenv").config(); //enabled dotenv
+
 const express = require("express");
 // import express from 'express';
 
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world !");
+});
 
 module.exports = app; // no need to enable module in package in json
